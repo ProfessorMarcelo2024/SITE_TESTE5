a13d278f299860d39ae026b53479ff361cba16cf
gersonbeljr <gersonbeljr@gmail.com>
2024-04-09 17:05:39 -0300

    adição dos primeiros passos do Js do bot

---
+++ Main/chatbot.js
@@ -0,0 +1,21 @@
+//chatBot script
+const chatBotText = document.querySelector('#chatPergunta');
+const chatBotEnv = document.querySelector("#enviar");
+
+console.log("ta pegando")
+
+chatBotEnv.addEventListener('click',()=>{
+    let mensagem = chatBotText.Value;
+})
+
+function criaTexto(mensagem) {
+    
+}
+
+
+//<div class="textChatContainer">
+/* <img src="imgs/petrologoRound.svg" alt="">
+<div class="textoConfig textoBot">
+    <p>mensagem</p>
+</div>
+</div> */
