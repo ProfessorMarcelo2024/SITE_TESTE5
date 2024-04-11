a08e97a4007eded25761c4c8f60311bba5dbefbb
gersonbeljr <gersonbeljr@gmail.com>
2024-04-11 16:00:44 -0300

    Chat moldado

--- Main/chatbot.js
+++ Main/chatbot.js
@@ -5,18 +5,19 @@ const chatBotMess = document.querySelector('#botChat');
 
 chatBotEnv.addEventListener('click', () => {
     let mensagem = 
-    `<div class='textChatContainer'>
-        <img src='imgs/petrologoRound.svg' alt=''>
-        <div class='textoConfig textoBot'>
+    `
+        <div class='textoConfig textoUsuario'>
             <p>${chatBotText.value}</p >
         </div >
-    </div >`;
-    document.chatBotMess.appendChild(mensagem);
+        <img src="imgs/botIcon.svg" alt="">
+   `;
+    const div=document.createElement('div')
+    div.setAttribute('class','textChatContainer');
+    div.innerHTML = mensagem;
+    chatBotMess.appendChild(div);
 })
 
-function criaTexto(mensagem) {
-    
-}
+
 
 
 //<div class='textChatContainer'>
