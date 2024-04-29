1f175e89b377498d654778726051fca3f735064d
unknown <gersonbeljr>
2024-04-29 16:03:33 -0300

    commit da tarde

--- Main/estilos/ScriptEstilo.js
+++ Main/estilos/ScriptEstilo.js
@@ -1,4 +1,19 @@
+//window track adjust to mobile
 window.addEventListener('resize',()=>{
     let altura = window.innerHeight*0.01;
     document.documentElement.style.setProperty('--vh',`${altura}px`);
 })
+
+//Open and close bot
+const chatIntro = document.querySelector('.chatConjunto');
+const modalbot = document.querySelector('.formModal');
+const btnCloseBot = document.querySelector('#fecharBot');
+//Open 
+chatIntro.addEventListener('click',()=>{
+    modalbot.style.display = 'block';
+})
+//close
+btnCloseBot.addEventListener('click',()=>{
+    modalbot.style.display = 'none'
+})
+
